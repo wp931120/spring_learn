fe4001c415c13099d6b823b9b72bc3de3033715b
matt.wang <matt.wang@gaodun.com>
2020-01-07 20:28:46 +0800

    impliment bubble sort

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# The Project is for  Java Practicing
+### the first part is about basic language grammar
--- src/main/java/com/gaodun/spring/App.java
+++ src/main/java/com/gaodun/spring/App.java
@@ -4,10 +4,33 @@ package com.gaodun.spring;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
+        int[] array = {1,312,31,2,3};
+        bubsort(array);
+
         System.out.println( "Hello World!" );
+    }
+
+
+    public static void bubsort(int[] array){
+        /**
+         * 实现冒泡排序
+         */
+//        System.out.println(array.length);
+        for (int i=0;i<array.length;i++){
+            for (int j=0;j<array.length-i-1;j++){
+                if (array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+
+                }
+            }
+        }
+        for(int num:array) {
+            System.out.println(num);
+        }
     }
 }
