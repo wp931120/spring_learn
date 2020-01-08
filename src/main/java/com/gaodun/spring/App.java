e3628ffc39f895c5c4ea56726afd6d92faedde23
matt.wang <matt.wang@gaodun.com>
2020-01-08 16:21:59 +0800

    add some sort Algorithm

--- src/main/java/com/gaodun/spring/App.java
+++ src/main/java/com/gaodun/spring/App.java
@@ -1,5 +1,7 @@
 package com.gaodun.spring;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -7,9 +9,17 @@ package com.gaodun.spring;
 public class App {
     public static void main( String[] args )
     {
-        int[] array = {1,312,31,2,3};
-        bubsort(array);
-
+        int[] array0 = {1,312,31,2,3};
+        bubsort(array0);
+        int[] array1 = {1,123,12,2,3};
+        selectSort(array1);
+        int[] array2 = {1,312,1,12,3};
+        insertSort(array2);
+        int[] array3 = {123,412,12,312,31};
+        int start = 0;
+        int end = array3.length-1;
+        quickSort(array3,start,end);
+        System.out.println(Arrays.toString(array3));
         System.out.println( "Hello World!" );
     }
 
@@ -29,8 +39,81 @@ public class App {
                 }
             }
         }
-        for(int num:array) {
-            System.out.println(num);
-        }
+        System.out.println(Arrays.toString(array));
+
     }
+
+    public static void selectSort(int[] array){
+        /**
+         * 实现选择排序
+         */
+        if (array.length == 0){
+            System.out.println(Arrays.toString(array));
+            return;
+        }
+        for (int i=0;i<array.length;i++){
+            int minIndex = i;
+            for (int j=i;j<array.length;j++){
+                if (array[j]<array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int tmp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = tmp;
+
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void insertSort(int[] array){
+        /**
+         *插入排序
+         */
+        if (array.length ==0){
+            return;
+        }
+        for (int i= 0; i< array.length-1 ;i++){
+//            int cur = array[i+1];
+            for(int j = i; j >= 0;j--){
+                if (array[j+1]<array[j]){
+                    int tmp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+
+
+    }
+
+    public static void quickSort(int[] array,int start ,int end){
+
+        if (array.length < 0){
+            return;
+        }
+        if (start>end){
+            return;
+        }
+        int left = start;
+        int right = end;
+        int mid = array[left];
+        while (left<right){
+            while (left<right && array[right]>mid){
+                right --;
+            }
+            array[left] = array[right];
+            while (left<right && array[left]<mid){
+                left ++;
+            }
+            array[right] = array[left];
+        }
+        array[left] = mid;
+//        System.out.println(Arrays.toString(array));
+        quickSort(array,start,left-1);
+        quickSort(array,right+1,right);
+
+    }
+
 }
